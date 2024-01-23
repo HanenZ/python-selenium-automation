7c6a7034895508647d4d41e8c1a0099481fc71b8
unknown <zoghlami_hanen@yahoo.fr>
2024-01-23 08:59:11 +0100

    updated automated search script

--- sample_script.py
+++ sample_script.py
@@ -14,11 +14,14 @@ driver.maximize_window()
 
 # open the url
 driver.get('https://www.google.com/')
+sleep(2)
+#accept the cookies
+driver.find_element(By.CSS_SELECTOR, '[class="QS5gu sy4vM"]').click()
 
 # populate search field
 search = driver.find_element(By.NAME, 'q')
 search.clear()
-search.send_keys('Car')
+search.send_keys('computer')
 
 # wait for 4 sec
 sleep(4)
@@ -27,7 +30,7 @@ sleep(4)
 driver.find_element(By.NAME, 'btnK').click()
 
 # verify search results
-assert 'car' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
+assert 'computer' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
 print('Test Passed')
 
 driver.quit()
